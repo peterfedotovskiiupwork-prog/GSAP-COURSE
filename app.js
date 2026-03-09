ce5b16ab40128d1e45771b9319dd02d7d9cce587
peterfedotovskiiupwork-prog <peter.fedotovskii.upwork@gmail.com>
2026-03-09 16:39:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const Firstname = " Im peter"
+const age = " Im 17yo"
+const town = " Living in Stuttgart"
+const hobby = " My hobby is carving wood"
+console.log("About me:", Firstname, age, town, hobby)
