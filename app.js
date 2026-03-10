5db9dd951aa9d39ed2273149b03cf5d56f48f958
peterfedotovskiiupwork-prog <peter.fedotovskii.upwork@gmail.com>
2026-03-10 20:57:32 +0100

    2.2 PRO

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const Firstname = " Im peter"
-const age = " Im 17yo"
-const town = " Living in Stuttgart"
-const hobby = " My hobby is carving wood"
-console.log("About me:", Firstname, age, town, hobby)
+const titleProject = "Оценка стоимости"
+const screensValue = "шаблонные, с уникальным дизайном, с анимациями"
+const screenPrice = "500$"
+const percentage = "25%"
+const fullPrice = "1500$"
+const responsive = "true"
+
+console.log(titleProject)
+console.log(screensValue)
+console.log(screenPrice)
+console.log(percentage)
+console.log(fullPrice)
+console.log(responsive)
+
+
+
